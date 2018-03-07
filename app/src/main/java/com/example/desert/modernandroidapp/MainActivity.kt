51cb68273e00a40038c368c3029b51e76b8db3df
Quentin Désert <quentin.desert@schibsted.com>
2018-03-07 16:34:31 -0600

    Added Repository class and use it to display repo

--- app/src/main/java/com/example/desert/modernandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/desert/modernandroidapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.desert.modernandroidapp
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.desert.modernandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            repositoryName.text = "Android repositories app"
+            repositoryOwner.text = "Quentin"
+            numberOfStars.text = "1000 stars"
+        }
+
+        var repository = Repository("Android repos app", "Q", 697, true)
+
+        binding.repository = repository;
+        binding.executePendingBindings()
+
+        Handler().postDelayed({
+            repository.repositoryName = "New Name"
+            binding.repository = repository;
+            binding.executePendingBindings()
+        }, 2000)
     }
 }
---
+++ app/src/main/java/com/example/desert/modernandroidapp/Repository.kt
@@ -0,0 +1,3 @@
+package com.example.desert.modernandroidapp
+
+class Repository (var repositoryName: String?, var repositoryOwner: String?, var numberOfStars: Int?, var hasIssues: Boolean = false)
