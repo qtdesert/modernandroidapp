499c4d987395fb74a1ae2c5173f915be8b3ba741
Quentin Désert <quentin.desert@schibsted.com>
2018-03-07 12:15:18 -0600

    Integrated Data binding library

--- app/src/main/java/com/example/desert/modernandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/desert/modernandroidapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.desert.modernandroidapp
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desert.modernandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
