35ced9884f72aea1f87adf70fdb52e8476f3db16
Quentin Désert <quentin.desert@schibsted.com>
2018-04-09 11:37:51 -0500

    Added net manager

--- app/src/main/java/com/example/desert/modernandroidapp/data/GitRepoRepository.kt
+++ app/src/main/java/com/example/desert/modernandroidapp/data/GitRepoRepository.kt
@@ -1,19 +1,30 @@
 package com.example.desert.modernandroidapp.data
 
+import com.example.desert.modernandroidapp.androidmanagers.NetManager
 import com.example.desert.modernandroidapp.ui.uimodels.Repository
 
-class GitRepoRepository {
+class GitRepoRepository(val netManager: NetManager) {
 
     val localDataSource = GitRepoLocalDataSource()
     val remoteDataSource = GitRepoRemoteDataSource()
 
     fun getRepositories(onRepositoryReadyCallback: OnRepositoryReadyCallback) {
-        remoteDataSource.getRepositories( object : OnRepoRemoteReadyCallback {
-            override fun onRemoteDataReady(data: ArrayList<Repository>) {
-                localDataSource.saveRepositories(data)
-                onRepositoryReadyCallback.onDataReady(data)
+        netManager.isConnectedToInternet?.let {
+            if (it) {
+                remoteDataSource.getRepositories(object : OnRepoRemoteReadyCallback {
+                    override fun onRemoteDataReady(data: ArrayList<Repository>) {
+                        localDataSource.saveRepositories(data)
+                        onRepositoryReadyCallback.onDataReady(data)
+                    }
+                })
+            } else {
+                localDataSource.getRepositories(object : OnRepoLocalReadyCallback {
+                    override fun onLocalDataReady(data: ArrayList<Repository>) {
+                        onRepositoryReadyCallback.onDataReady(data)
+                    }
+                })
             }
-        })
+        }
     }
 }
 
--- app/src/main/java/com/example/desert/modernandroidapp/ui/screens/MainViewModel.kt
+++ app/src/main/java/com/example/desert/modernandroidapp/ui/screens/MainViewModel.kt
@@ -1,18 +1,20 @@
 package com.example.desert.modernandroidapp.ui.screens
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
+import com.example.desert.modernandroidapp.androidmanagers.NetManager
 import com.example.desert.modernandroidapp.data.GitRepoRepository
 import com.example.desert.modernandroidapp.ui.uimodels.Repository
 import com.example.desert.modernandroidapp.data.OnRepositoryReadyCallback
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    var gitRepoRepository: GitRepoRepository = GitRepoRepository()
+    var gitRepoRepository: GitRepoRepository = GitRepoRepository(NetManager(getApplication()))
 
-    val text = ObservableField<String>()
-    val isLoading = ObservableField<Boolean>()
+    val text = ObservableField("old data")
+    val isLoading = ObservableField(false)
 
     var repositories = MutableLiveData<ArrayList<Repository>>()
 
